c7fe079dac3c4f31fe1cefae732c1263100d20b7
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2022-04-26 12:32:50 +0530

    🇮🇳

--- day4/pythonProject/test_maths_utils.py
+++ day4/pythonProject/test_maths_utils.py
@@ -14,6 +14,23 @@ class TestMathsUtils(unittest.TestCase):
         expected = 30.4
         self.assertEqual(actual, expected)
 
+    def test_add_numbers_with_strings(self):
+        actual = maths_utils.calculate_sum('10.2, 20.2, vinod, False, 10')
+        expected = 40.4
+        self.assertEqual(actual, expected)
+
+    def test_raise_value_error_for_number_input(self):
+        try:
+            maths_utils.calculate_sum(123)
+            self.fail('Expected ValueError to be raised. But did not receive any!')
+        except ValueError:
+            # test passes; nothing to be done her
+            pass
+
+    def test_raise_value_error_for_bool_input(self):
+        with self.assertRaises(ValueError):
+            maths_utils.calculate_sum(False)
+
 
 if __name__ == '__main__':
     unittest.main()
